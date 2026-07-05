48e88617d5e17adb6bc9a09239690c5ee355f195
Amrita <muskanaryamuskanarya167@gmail.com>
2026-07-05 09:49:34 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,19 @@
+"use strict"//tread js as newer version
+
+//alert(3+3)//we are useing nodejs not browser
+
+console.log(3+
+    3);//code readability should be high
+    console.log(3+3);
+
+    //datatypes
+    let name="amrita";//string
+    let age=18//number 
+    let islogin=false//boolen
+    let isnull=null//null
+    let state;//undefine
+
+    //object
+
+    let myage=19
+    console.log(typeof age);
