7d6e0ca8d25afc7fe2272ea81729bac198872d46
Amrita <muskanaryamuskanarya167@gmail.com>
2026-07-08 16:35:29 +0000

    dom learning

---
+++ dom/dom.js
@@ -0,0 +1,20 @@
+// 1. Heading select karo
+const heading = document.getElementById("heading");
+
+// 2. Heading ka text change karo
+
+// 3. Paragraph ka color change karo
+document.getElementById("para").style.color = "red";
+
+// 4. Image change karo
+document.getElementById("image").src = "https://picsum.photos/250";
+
+// 5. Button par click hone par alert dikhao
+document.getElementById("btn").addEventListener("click", function () {
+    alert("Button Clicked!");
+});
+para.innerText="abc";
+let heading = document.querySelector('h1');
+heading.innerText;
+heading.innerHTML="<u>Welcome to DOM</u>";
+heading
